1111fdf4cad8a43336b99da5caea683352afb777
Jupalaja <jupalaja@gmail.com>
2022-10-12 20:36:08 -0500

    Error resuelto, función leerDatos creada usando punteros y llamadas por referencia

--- laboratorio_1/reto1.c
+++ laboratorio_1/reto1.c
@@ -1,43 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double *leerDatos();
+/*
+    Declaración de la función usando un puntero para que pueda ser definida después de main.
+*/
+void *leerDatos(int* n, double** nums_x, double** nums_y);
 
 int main() {
     printf("Reto 1 en progreso...\n");
-    double *numbers = leerDatos(); //Leemos 2 datos
-    printf("The elements of the array are: ");
-    for (int i = 0; i < sizeof numbers; ++i) {
-        printf("%f, ", numbers[i]);
+    int a;
+    double *numbersX, *numbersY;
+
+    /*
+        Llamada a la función por referencia usando punteros para retornar multiples valores
+        y para trabajar sobre las variables originales sin copiarlas, ahorrando memoria.
+    */
+    leerDatos(&a, &numbersX, &numbersY);
+    for (int i = 0; i < a; ++i) {
+        printf("x%d: %.4f    y%d: %.4f\n", i, numbersX[i], i, numbersY[i]);
     }
     return 0;
 }
 
-// Función para ingresar los datos iniciales
-double *leerDatos() {
-    int n; //Variable para recibir el número de parejas de datos
-    char num_in[5]; //Variable para recibir los datos de consola
-    double *nums_x; //Variable para almacenar los valores de
+/*
+    Para la función leerDatos utilizamos un puntero [int* n] para obtener el valor del tamaño de los
+    arreglos, y punteros [double** nums_x] y [double** nums_y] para asignar memoria variable y hacer
+    llamadas por referencia en la función main.
+*/
+void *leerDatos(int* n, double** nums_x, double** nums_y) {
+    char num_in[sizeof(double)]; // Variable para recibir en formato texto desde la consola
 
     printf("Ingrese la longitud de los arreglos X y Y: \n");
-    scanf("%d", &n); //Escaneo de la entrada del teclado
-    printf("Ingreso %d números. \n", n);
+    scanf("%d", n); // Escaneamos el valor de n: el tamaño de los arreglos a usar
+    printf("Ingreso %d números. \n", *n);
 
-    double *p = (double *)malloc(n * sizeof(double));
-    printf("%lu\n",sizeof(*p));
+    /*
+        Debemos asignar suficiente memoria para los vectores X y Y, a saber: n x [Tamaño de double].
+    */
+    *nums_x = malloc(*n * sizeof(double));
+    *nums_y = malloc(*n * sizeof(double));
 
     printf("Ingrese Los valores de x: \n");
-    for(int i=0; i<n;i=i+1)     {
-        scanf("%s",num_in); //Escaneo de la entrada del teclado
-        nums_x[i]=atof(num_in); //Conversión de string a double
+    /*
+        Guardamos el valor almacenado en la variable temporal [num_in] en la posición correspondiente
+        a nuestros punteros/arreglos [*nums_x] y [*nums_y].
+    */
+    for(int i=0; i<*n;i=i+1)     {
+        scanf("%s",num_in);
+        *(*nums_x+i) = strtod(num_in, NULL);
     }
-
-//    printf("Ingrese Los valores de y: \n");
-//    for(int j=num_par; j < num_par * 2; j= j + 1)     {
-//        scanf("%s",num_in); //Escaneo de la entrada del teclado
-//        nums_x[j]=atof(num_in); //Conversión de string a double
-//    }
-    return nums_x;
+    printf("Ingrese Los valores de y: \n");
+    for(int i=0; i<*n;i=i+1)     {
+        scanf("%s",num_in);
+        *(*nums_y+i) = strtod(num_in, NULL);
+    }
+    return NULL;
 }
+
 
 
