21109322df5c49c1c5510fa026ea3520c64f3114
Jupalaja <jupalaja@gmail.com>
2022-10-14 11:59:14 -0500

    Función de minimos cuadrados terminada

--- laboratorio_1/reto1.c
+++ laboratorio_1/reto1.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /*
     Declaración de la función usando un puntero para que pueda ser definida después de main.
 */
 void leerDatos(int* n, double** nums_x, double** nums_y);
-void minimosCuadrados(int n, const double* nums_x, const double* nums_y, double *m, double *b, double *r);
+void minimosCuadrados(int N, const double* nums_x, const double* nums_y, double *m, double *b, double *r);
 
 int main() {
     printf("Reto 1 en progreso...\n");
-    int a;
+    int n;
     double *numbersX, *numbersY;
-    double sumx, sumy, sumxy;
+    double m = 0, b = 0, r = 0;
     /*
         Llamada a la función por referencia usando punteros para retornar multiples valores
         y para trabajar sobre las variables originales sin copiarlas, ahorrando memoria.
     */
-    leerDatos(&a, &numbersX, &numbersY);
-    for (int i = 0; i < a; ++i) {
-        printf("x%d: %.4f    y%d: %.4f\n", i, numbersX[i], i, numbersY[i]);
+    leerDatos(&n, &numbersX, &numbersY);
+    printf("Valores Iniciales:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("x%d: %.4f\ty%d: %.4f\n", i, numbersX[i], i, numbersY[i]);
     }
-    minimosCuadrados(a, numbersX, numbersY, &sumx, &sumy, &sumxy);
-    printf("%.4f\n", sumx);
-    printf("%.4f\n", sumy);
-    printf("%.4f\n", sumxy);
+    minimosCuadrados(n, numbersX, numbersY, &m, &b, &r);
+
+    if(isnan(m) || isnan(b) || isnan(r)) {
+        printf("\nError: se han obtenido valores indeterminados, verifique los parámetros iniciales.\n");
+    } else {
+        printf("\nParámetros de salida:\n");
+        printf("m = %.4f\n", m);
+        printf("b = %.4f\n", b);
+        printf("r = %.4f\n", r);
+        printf("\nFunción resultante: \t");
+        printf("y = %.4f*x + %.4f, Correlación: %.4f \n", m,b,r);
+    }
+
     return 0;
 }
 
@@ -60,23 +71,19 @@ void leerDatos(int* n, double** nums_x, double** nums_y) {
     }
 }
 
-void minimosCuadrados(int n, const double* nums_x, const double* nums_y, double* m, double* b, double* r){
-    double sumX = 0;
-    double sumY = 0;
-    double sumXY = 0;
+void minimosCuadrados(int N, const double* nums_x, const double* nums_y, double* m, double* b, double* r){
+    double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0, sumY2 = 0;
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < N; ++i) {
         sumX += *(nums_x+i);
-    }
-    for (int i = 0; i < n; ++i) {
         sumY += *(nums_y+i);
-    }
-    for (int i = 0; i < n; ++i) {
         sumXY += (*(nums_x+i))*(*(nums_y+i));
+        sumX2 += (*(nums_x+i))*(*(nums_x+i));
+        sumY2 += (*(nums_y+i))*(*(nums_y+i));
     }
-    *m = sumX;
-    *b = sumY;
-    *r = sumXY;
+    *m = (N*sumXY - sumX*sumY)/(N*sumX2 - (pow(fabs(sumX),2)));
+    *b = (sumY*sumX2 - sumX*sumXY)/(N*sumX2 - (pow(fabs(sumX),2)));
+    *r = (N*sumXY - sumX*sumY)/(sqrt((N*sumX2 - pow(sumX,2))*(N*sumY2 - pow(sumY,2))));
 }
 
 
