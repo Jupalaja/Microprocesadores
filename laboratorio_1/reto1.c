2e76031c19cdf526a81b25349188310614491fb2
Jupalaja <jupalaja@gmail.com>
2022-10-15 05:33:58 -0500

    Menú inicial actualizado, función modoArchivo terminada.

--- laboratorio_1/reto1.c
+++ laboratorio_1/reto1.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#define MAXCHAR 1000
 
-void leerDatos(int* n, double** nums_x, double** nums_y);
-void minimosCuadrados(int N, const double* nums_x, const double* nums_y, double *m, double *b, double *r);
+
 void modoManual(int* n, double** nums_x, double** nums_y);
+void modoArchivo(int* n, double** nums_x, double** nums_y);
+void minimosCuadrados(int N, const double* nums_x, const double* nums_y, double *m, double *b, double *r);
 
 int main() {
     printf("Reto 1 en progreso...\n");
@@ -17,42 +21,46 @@ int main() {
         [b]: Corte con el eje Y
         [r]: Correlación de Pearson
     */
-    int N, modo;
+    int N, modo=0;
     double *numbersX, *numbersY;
     double m = 0, b = 0, r = 0;
 
-    printf("Seleccione un Modo de Ingreso de datos: "
-           "\n·)  Ingrese 1 para Ingreso Manual de datos"
-           "\n·)  Ingrese 2 para Ingreso de datos desde Archivo\n\t");
-    scanf("%d", &modo);
-    if (modo == 1) {
-        printf("Seleccionó Modo Manual\n");
-        /*
-           Llamada a las funciones pasando parámetros por referencia [&parámetro] usando punteros para
-           retornar multiples valores y trabajar sobre las variables originales, ahorrando memoria.
-       */
-        modoManual(&N, &numbersX, &numbersY);
-        minimosCuadrados(N, numbersX, numbersY, &m, &b, &r);
-    } else if (modo == 2) {
-        printf("Modo archivo en progreso...");
-    } else {
-        printf("Opción no valida");
-    }
+    /*
+        Menú para Seleccionar el tipo de entrada de datos, la llamada a las funciones se realiza
+        pasando parámetros por referencia utilizando punteros, optimizando el uso de la memoria.
+    */
+    do {
+        printf("\nSeleccione un Modo de Ingreso de datos: "
+               "\n·)  Ingrese 1 para Ingreso Manual de datos"
+               "\n·)  Ingrese 2 para Ingreso de datos desde Archivo"
+               "\n·)  Ingrese 3 para salir\n\t");
+        scanf("%d", &modo);
+        switch (modo) {
+            case 1:
+                printf("Seleccionó Modo Manual\n");
+                modoManual(&N, &numbersX, &numbersY);
+                minimosCuadrados(N, numbersX, numbersY, &m, &b, &r);
+                break;
+            case 2:
+                printf("Seleccionó Modo Archivo\n");
+                modoArchivo(&N, &numbersX, &numbersY);
+                minimosCuadrados(N, numbersX, numbersY, &m, &b, &r);
+                break;
+            case 3:
+                printf("Hasta Pronto :)");
+                break;
+            default:
+                printf("Opción no valida");
+        }
+    } while (modo != 3);
     return 0;
 }
-void modoManual(int* n, double** nums_x, double** nums_y){
-    leerDatos(n, nums_x, nums_y);
-    printf("Valores Iniciales:\n");
-    for (int i = 0; i < *n; ++i) {
-        printf("x%d: %.2f\t\ty%d: %.2f\n", i, *(*nums_x+i), i, *(*nums_y+i));
-    }
-}
+
 /*
-    En esta función utilizamos un puntero [*n] para obtener el valor del tamaño de los arreglos
-    y punteros dobles [**nums_x] y [**nums_y] para asignar memoria variable y hacer llamadas por
-    referencia en la función main.
+    Esta función utiliza un puntero [*n] para obtener el valor del tamaño de los arreglos
+    y punteros dobles [**nums_x] y [**nums_y] para crear vectores que pasaremos por referencia.
 */
-void leerDatos(int* n, double** nums_x, double** nums_y) {
+void modoManual(int* n, double** nums_x, double** nums_y){
     /*
         Escaneamos el valor de n: el tamaño de los arreglos a usar y lo almacenamos en [num_in].
     */
@@ -78,9 +86,71 @@ void leerDatos(int* n, double** nums_x, double** nums_y) {
         printf("Ingrese el valor de y%d: ", i);
         scanf("%s",num_in);
         *(*nums_y+i) = strtod(num_in, NULL);
-        printf("\n"); // Salto de linea
+        printf("\n");
+    }
+    printf("Valores Iniciales:\n");
+    for (int i = 0; i < *n; ++i) {
+        printf("x%d: %.2f\t\ty%d: %.2f\n", i, *(*nums_x+i), i, *(*nums_y+i));
     }
 }
+
+/*
+    Esta función recibe un los mismos parámetros que modoManual; sin embargo, utilizamos funciones
+    de la librería [<string.h>] para leer renglones en el archivo csv y obtener los valores desde alli.
+*/
+void modoArchivo(int* n, double** nums_x, double** nums_y){
+    /*
+        Declaramos las variables a usar:
+        [*fp]: Variable para almacenar nuestro archivo csv de entrada
+        [row]: Arreglo que contiene un renglón leído completo.
+        [token]: Variable para separar y obtener los valores individuales de X y Y de cada renglón
+        [count]: variable para contar los renglones leídos y hallar el valor de n
+    */
+    FILE* fp;
+    char row[MAXCHAR];
+    char nombreArchivo[20];
+    char* token;
+    int count = 0;
+
+    /*
+        Solicitamos al usuario el nombre del archivo a leer, ubicado en la sub-carpeta [/data]
+        para ser almacenado.
+    */
+    printf("Ingrese el nombre del archivo (Incluya la extensión):\n");
+    scanf("%s", nombreArchivo);
+
+    /*
+        Abrimos la primera vez los datos en nuestro archivo para conocer el número de
+        vectores dado, después asignamos la memoria correspondiente a nuestros vectores [nums_x] y [num_y].
+    */
+    fp = fopen(nombreArchivo,"r");
+    if (!fp){
+        printf("No se pudo abrir el archivo\n");
+    }
+    while (feof(fp) != true) {
+        fgets(row, MAXCHAR, fp);
+        count ++;
+    }
+    *n = count;
+    *nums_x = malloc(*n * sizeof(double));
+    *nums_y = malloc(*n * sizeof(double));
+
+    /*
+        Recorremos una segunda vez los datos en nuestro archivo para obtener los valores de X y Y de
+        cada renglón y pasarlos a los vectores [nums_x] y [num_y].
+    */
+    fp = fopen(nombreArchivo,"r");
+    int i = 0;
+    while (feof(fp) != true) {
+        fgets(row, MAXCHAR, fp);
+        token = strtok(row, ",");
+        *(*nums_x+i) = strtod(token, NULL);
+        token = strtok(NULL, ",");
+        *(*nums_y+i) = strtod(token, NULL);
+        i ++;
+    }
+}
+
 /*
     Esta función recibe como parámetros de entrada el valor de [N], y los vectores [nums_x] y [nums_y]
     para calcular los valores de [m], [b], y [r] en punteros que serán pasados por referencia.
@@ -88,8 +158,8 @@ void leerDatos(int* n, double** nums_x, double** nums_y) {
 void minimosCuadrados(int N, const double* nums_x, const double* nums_y, double* m, double* b, double* r){
     double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0, sumY2 = 0;
     /*
-        Mediante un ciclo for obtenemos todas las variables necesarias para calcular las variables de
-        salida, luego reemplazamos dichos valores según la regresión lineal de mínimos cuadrados.
+        Mediante un ciclo for obtenemos todas los valores necesarios para calcular las variables de
+        salida, luego reemplazamos dichos valores según la función de regresión lineal de mínimos cuadrados.
     */
     for (int i = 0; i < N; ++i) {
         sumX += *(nums_x+i);
